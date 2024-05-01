431520c1c22607a4e54c714d011e3bfa42b9e34a
David Whitlow <david.whitlow@r2c.dev>
2024-05-01 16:01:35 -0700

    update pipe

--- check_prs.py
+++ check_prs.py
@@ -1,12 +1,13 @@
 import os
 from azure.devops.connection import Connection
+from azure.devops.v6_0.git.git_client import GitClient
+from azure.devops.v6_0.git.models import GitPullRequestSearchCriteria
 from msrest.authentication import BasicAuthentication
 
 def main():
     # Obtain the necessary variables from environment variables
     organization_url = os.environ['SYSTEM_TEAMFOUNDATIONCOLLECTIONURI']
     project_name = os.environ['SYSTEM_TEAMPROJECT']
-    repository_id = os.environ['BUILD_REPOSITORY_ID']
     source_branch = os.environ['BUILD_SOURCEBRANCHNAME']
     personal_access_token = os.environ['SYSTEM_ACCESSTOKEN']
 
@@ -17,13 +18,16 @@ def main():
     # Get a client (the Git client provides access to pull requests)
     git_client = connection.clients.get_git_client()
 
+    # Prepare search criteria for pull requests
+    search_criteria = GitPullRequestSearchCriteria(
+        source_ref_name=f'refs/heads/{source_branch}',
+        status='active'
+    )
+
     # List pull requests
     pull_requests = git_client.get_pull_requests_by_project(
-        project_id=project_name,
-        search_criteria={
-            'sourceRefName': f'refs/heads/{source_branch}',
-            'status': 'active'
-        }
+        project=project_name,
+        search_criteria=search_criteria
     )
 
     # Check if there are any open pull requests for the branch
